c95b785a7fecc8030710e3a64a5205008225f783
Ahmed Hassan <a.hassan@buismed.de>
2014-11-28 12:32:33 +0100

    corrected version number to have 3 digits

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'flowkey:hotkeys',
   summary: 'Easy and effective hotkeys for your app, powered by Mousetrap',
-  version: '1.1',
+  version: '1.1.0',
   git: 'https://github.com/nerdmed/hotkeys.git'
 });
 
