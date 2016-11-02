daddb226ab5b6d458f96cf28b24ecd09bb0bcec1
Ahmed Hassan <a.hassan@buismed.de>
2016-11-02 11:04:43 +0100

    added changelog and bump version

---
+++ Changelog.md
@@ -0,0 +1,5 @@
+#Changelog
+
+
+## v1.2
+- added eventType option
--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'flowkey:hotkeys',
   summary: 'Easy and effective hotkeys for your app, powered by Mousetrap',
-  version: '1.1.1',
+  version: '1.2.0',
   git: 'https://github.com/nerdmed/hotkeys.git'
 });
 
